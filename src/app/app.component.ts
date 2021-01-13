bb193b547de0cc4ebe429184e4dd389be6d6aaab
Joseph Patambag <joseph@smartpods.ca>
2021-01-13 08:57:17 -0400

    source reupdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,6 @@ export class AppComponent {
   customers: any = [];
   constructor(private exportService: ExportService) { }
 
-  // ngOnInit() {
-  //   for (let i = 0; i <= 25; i++) {
-  //     this.customers.push({firstName: `first${i}`, lastName: `last${i}`,
-  //     email: `abc${i}@gmail.com`, address: `000${i} street city, ST`, zipcode: `0000${i}`});
-  //   }
-  // }
-
-  // tslint:disable-next-line:typedef
   export() {
     this.exportService.exportExcel(this.customers, 'customers');
   }
